c7836e5986ae6be5ebc223afad209e81ff762073
weslybrandao <brandaowesly@gmail.com>
2023-07-01 02:17:14 -0300

    BannerGrid styling

--- components/ui/BannerGrid.tsx
+++ components/ui/BannerGrid.tsx
@@ -32,13 +32,14 @@ export interface Banner {
   backgroundColor?: string;
 
   /**
+   * @description Label
+   */
+  label?: string;
+
+  /**
    * @description Text
    */
   text?: {
-    /**
-     * @description Title
-     */
-    title?: string;
     /**
      * @description Color
      */
@@ -63,6 +64,32 @@ export interface Banner {
      */
     badges?: BagdeItem[];
   };
+
+  /**
+   * @description Coluna inicial
+   */
+
+  columnStart?: number;
+
+  /**
+   * @description Linha inicial
+   */
+
+  rowStart?: number;
+
+  /**
+   * @description Quantidade de linhas que irá ocupar
+   */
+
+  rowSpan?: number;
+
+  /**
+   * @description Quantidade de colunas que irá ocupar
+   */
+
+  colSpan?: number;
+
+  hoverUrl?: string;
 }
 
 export type BorderRadius =
@@ -86,6 +113,16 @@ export interface Props {
     desktop?: 1 | 2 | 3 | 4 | 6 | 8;
   };
   /**
+   * @description Columns per line
+   */
+  itemsColumn: {
+    /** @default 2 */
+    mobile?: 1 | 2;
+    /** @default 4 */
+    desktop?: 1 | 2 | 3 | 4 | 6 | 8;
+  };
+
+  /**
    * @description Item's border radius in px
    */
   borderRadius: {
@@ -94,6 +131,14 @@ export interface Props {
     /** @default none */
     desktop?: BorderRadius;
   };
+
+  columnGap: {
+    /** @default 2 */
+    mobile?: 1 | 2;
+    /** @default 4 */
+    desktop?: 1 | 2 | 3 | 4 | 6;
+  };
+
   /** @max 4 */
   banners: Banner[];
 }
@@ -112,81 +157,147 @@ export const DESKTOP_COLUMNS: Record<number, string> = {
   8: "sm:grid-cols-8",
 };
 
+export const GRID_ROWS: Record<number, string> = {
+  1: "grid-rows-1",
+  2: "grid-rows-2",
+  3: "grid-rows-3",
+  4: "grid-rows-4",
+  5: "grid-rows-5",
+  6: "grid-rows-6",
+};
+
+export const GRID_COLUMNS: Record<number, string> = {
+  1: "grid-cols-1",
+  2: "grid-cols-2",
+  3: "grid-cols-3",
+  4: "grid-cols-4",
+  5: "grid-cols-5",
+  6: "grid-cols-6",
+};
+
+export const GRID_COL_START: Record<number, string> = {
+  1: "col-start-1",
+  2: "col-start-2",
+  3: "col-start-3",
+  4: "col-start-4",
+  5: "col-start-5",
+  6: "col-start-6",
+};
+
+export const GRID_ROW_START: Record<number, string> = {
+  1: "row-start-1",
+  2: "row-start-2",
+  3: "row-start-3",
+  4: "row-start-4",
+  5: "row-start-5",
+  6: "row-start-6",
+};
+
+export const GRID_ROW_SPAN: Record<number, string> = {
+  1: "row-span-1",
+  2: "row-span-2",
+  3: "row-span-3",
+  4: "row-span-4",
+  5: "row-span-5",
+  6: "row-span-6",
+};
+
+export const GRID_COL_SPAN: Record<number, string> = {
+  1: "col-span-1",
+  2: "col-span-2",
+  3: "col-span-3",
+  4: "col-span-4",
+  5: "col-span-5",
+  6: "col-span-6",
+};
+
+export const GRID_GAP: Record<number, string> = {
+  1: "gap-1",
+  2: "gap-2",
+  3: "gap-3",
+  4: "gap-4",
+  5: "gap-5",
+  6: "gap-6",
+};
+
 export const RADIUS_MOBILE = {
-  "none": "rounded-none",
-  "sm": "rounded-sm",
-  "md": "rounded-md",
-  "lg": "rounded-lg",
-  "xl": "rounded-xl",
+  none: "rounded-none",
+  sm: "rounded-sm",
+  md: "rounded-md",
+  lg: "rounded-lg",
+  xl: "rounded-xl",
   "2xl": "rounded-2xl",
   "3xl": "rounded-3xl",
-  "full": "rounded-full",
+  full: "rounded-full",
 };
 
 export const RADIUS_DESKTOP = {
-  "none": "sm:rounded-none",
-  "sm": "sm:rounded-sm",
-  "md": "sm:rounded-md",
-  "lg": "sm:rounded-lg",
-  "xl": "sm:rounded-xl",
+  none: "sm:rounded-none",
+  sm: "sm:rounded-sm",
+  md: "sm:rounded-md",
+  lg: "sm:rounded-lg",
+  xl: "sm:rounded-xl",
   "2xl": "sm:rounded-2xl",
   "3xl": "sm:rounded-3xl",
-  "full": "sm:rounded-full",
-};
-
-const BANNER_LAYOUTS: Record<number, string> = {
-  1: '"banner-0"',
-  2: '"banner-0 banner-1"',
-  3: `
-    "banner-0 banner-1"
-    "banner-0 banner-2"
-    `,
-  4: `
-    "banner-0 banner-2"
-    "banner-1 banner-3"
-    `,
+  full: "sm:rounded-full",
 };
 
 const VERTICAL_ALIGNMENT: Record<string, string> = {
-  "Top": "top-0",
-  "Middle": "top-1/2 -translate-y-1/2",
-  "Bottom": "bottom-0",
+  Top: "top-0",
+  Middle: "top-1/2 -translate-y-1/2",
+  Bottom: "bottom-0",
 };
 
 export const HORIZONTAL_ALIGNMENT: Record<string, string> = {
-  "Left": "items-left",
-  "Center": "items-center",
-  "Right": "items-end",
+  Left: "items-left",
+  Center: "items-center",
+  Right: "items-end",
 };
 
 export default function BannnerGrid({
+  itemsColumn,
+  itemsPerLine,
+  columnGap,
   borderRadius,
   banners = [],
 }: Props) {
   return (
     <section class="w-full md:px-0 mx-auto py-24">
       <div
-        class={`flex flex-col lg:grid lg:grid-rows-2 gap-4 md:gap-6`}
-        style={{
-          gridTemplateAreas: BANNER_LAYOUTS[banners?.length ?? 1] as string,
-        }}
+        class={`flex flex-col items-center auto-cols-auto lg:grid  ${
+          GRID_GAP[columnGap?.desktop ?? 2]
+        } ${GRID_COLUMNS[itemsColumn?.desktop ?? 2]} ${
+          GRID_ROWS[itemsPerLine?.desktop ?? 1]
+        }`}
       >
-        {banners.map((
-          { href, srcMobile, alt, backgroundColor, text },
-          index,
-        ) => (
-          <div
-            class="overflow-hidden relative block"
-            style={{
-              gridArea: `banner-${index}`,
-            }}
-          >
-            <a
-              class="block relative"
-              href={href}
+        {banners.map(
+          (
+            {
+              href,
+              srcMobile,
+              alt,
+              backgroundColor,
+              text,
+              columnStart,
+              rowStart,
+              rowSpan,
+              colSpan,
+              label,
+              hoverUrl,
+            },
+            index
+          ) => (
+            <div
+              key={index}
+              class={`group overflow-hidden transition duration-300 brightness-100 hover:bg-blue-500 relative block gap-4
+              ${GRID_ROW_START[rowStart ?? 0]}
+              ${GRID_COL_START[columnStart ?? 0]}
+              ${GRID_ROW_SPAN[rowSpan ?? 0]}
+              ${GRID_COL_SPAN[colSpan ?? 0]}
+           `}
             >
-              {text
-                ? (
+              <a class="block relative" href={href}>
+                {text ? (
                   <div
                     class={`w-full absolute flex z-10 md:p-10 p-5  ${
                       VERTICAL_ALIGNMENT[text.verticalAlignment ?? "Top"]
@@ -195,49 +306,91 @@ export default function BannnerGrid({
                     }`}
                   >
                     <span
-                      class="text-[40px] md:text-7xl my-2 md:my-4 font-medium"
+                      class={`text-[40px] md:text-7xl my-2 md:my-4 font-medium`}
                       style={{
                         color: text?.color ? text.color : "#482BE7",
                       }}
                     >
-                      {text.title}
+                      {label}
                     </span>
-                    <ul class="flex flex-wrap gap-2 items-center justify-start">
-                      {text?.badges?.length
-                        ? text.badges.map((item) => (
-                          <li
-                            class="p-2 rounded-lg text-[10px] md:text-xs uppercase"
-                            style={{
-                              color: item.textColor ? item.textColor : "#fff",
-                              backgroundColor: item.color
-                                ? item.color
-                                : "#482BE7",
-                            }}
-                          >
-                            {item.title}
-                          </li>
-                        ))
-                        : null}
-                    </ul>
+                    <div class="flex justify-between">
+                      <ul class="flex flex-wrap gap-2 items-center justify-start">
+                        {text?.badges?.length
+                          ? text.badges.map((item) => (
+                              <li
+                                class={`p-2 rounded-lg text-[10px] md:text-xs uppercase`}
+                                style={{
+                                  color: item.textColor
+                                    ? item.textColor
+                                    : "#fff",
+                                  backgroundColor: item.color
+                                    ? item.color
+                                    : "#482BE7",
+                                }}
+                              >
+                                {item.title}
+                              </li>
+                            ))
+                          : null}
+                      </ul>
+                      <a
+                        href={hoverUrl ? hoverUrl : "/"}
+                        class="hidden border-2 text-white hover:text-dark-blue border-white transition duration-300 hover:border-light-green pt-3 pb-3 pl-8 pr-8 rounded-full items-center group-hover:flex hover:bg-light-green"
+                      >
+                        Conferir
+                        <svg
+                          class="ml-2"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 18 18"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g id="Icon">
+                            <path
+                              id="Vector"
+                              d="M3.75 9H14.25"
+                              stroke="white"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                            <path
+                              id="Vector_2"
+                              d="M9 3.75L14.25 9L9 14.25"
+                              stroke="white"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                          </g>
+                        </svg>
+                      </a>
+                    </div>
                   </div>
-                )
-                : null}
-              <img
-                class={`w-full h-full ${
+                ) : null}
+                <img
+                  class={`w-full h-full  ${
+                    RADIUS_MOBILE[borderRadius.mobile ?? "none"]
+                  } ${RADIUS_DESKTOP[borderRadius.desktop ?? "none"]} `}
+                  src={srcMobile}
+                  alt={alt}
+                  style={{
+                    backgroundColor,
+                  }}
+                  decoding="async"
+                  loading="lazy"
+                  preload=""
+                />
+              </a>
+              <div
+                class={`invisible transition duration-700 group-hover:visible w-full z-999 top-0 opacity-0 group-hover:opacity-60 h-full bg-default-blue absolute ${
                   RADIUS_MOBILE[borderRadius.mobile ?? "none"]
-                } ${RADIUS_DESKTOP[borderRadius.desktop ?? "none"]} `}
-                src={srcMobile}
-                alt={alt}
-                style={{
-                  backgroundColor,
-                }}
-                decoding="async"
-                loading="lazy"
-                preload=""
-              />
-            </a>
-          </div>
-        ))}
+                } ${RADIUS_DESKTOP[borderRadius.desktop ?? "none"]}`}
+              ></div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
