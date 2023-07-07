fdef7daf0c8e1bee71f84b2f522ec40edc27d5b0
viktormarinho <viktormpcs@gmail.com>
2023-07-07 19:33:58 -0300

    change cards classes to use daisyui

--- components/institutional/CardsContent.tsx
+++ components/institutional/CardsContent.tsx
@@ -26,28 +26,30 @@ function CardsContent({ cards }: Props) {
   return (
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-[30px] pb-12">
       {cards.map((card) => (
-        <div class="rounded-[10px] border-2 border-neutral-100 px-[30px] py-5 flex flex-col gap-[10px]">
-          <div class="flex items-center gap-[10px] h-6 text-emphasis">
-            <Icon id={card.heading.icon ?? "MapPin"} width={24} height={24} />
-            <h6 class="font-medium">{card.heading.title}</h6>
-          </div>
-          <div class="flex flex-col gap-[2px] text-sm text-base-300">
-            <span class="font-bold">{card.content.subtitle}</span>
-            {card.content.paragraphs &&
-              card.content.paragraphs.map((paragraph) => <p>{paragraph}</p>)}
-          </div>
-          <div class="flex flex-col gap-[6px] items-start font-bold text-base-content">
-            {card.links.map((link) => (
-              <a href={link.url} class="flex items-center gap-[10px] text-sm">
-                <Icon
-                  id={link.icon ?? "Phone"}
-                  width={18}
-                  height={18}
-                  strokeWidth={2}
-                />
-                <span>{link.label}</span>
-              </a>
-            ))}
+        <div class="card card-bordered border-2 border-neutral-100">
+          <div class="card-body">
+            <div class="flex items-center gap-[10px] h-6 text-emphasis">
+              <Icon id={card.heading.icon ?? "MapPin"} width={24} height={24} />
+              <h6 class="font-medium">{card.heading.title}</h6>
+            </div>
+            <div class="flex flex-col gap-[2px] text-sm text-base-300">
+              <span class="font-bold">{card.content.subtitle}</span>
+              {card.content.paragraphs &&
+                card.content.paragraphs.map((paragraph) => <p>{paragraph}</p>)}
+            </div>
+            <div class="flex flex-col gap-[6px] items-start font-bold text-base-content">
+              {card.links.map((link) => (
+                <a href={link.url} class="flex items-center gap-[10px] text-sm">
+                  <Icon
+                    id={link.icon ?? "Phone"}
+                    width={18}
+                    height={18}
+                    strokeWidth={2}
+                  />
+                  <span>{link.label}</span>
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       ))}
